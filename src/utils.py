b9c4139db0cd48c60ee425ffea022f285ac52ec7
Hayden Choi <hyj.choi@mail.utoronto.ca>
2024-11-30 18:27:58 -0500

    Integrate models into optimization

--- src/utils.py
+++ src/utils.py
@@ -7,3 +7,7 @@ def extract_coordinates(geocoded_city):
   # Look for patterns that start with ( and end with )
   match = re.search(r"\(([+-]?[0-9.]+,\s*[+-]?[0-9.]+)\)", str(geocoded_city)) # Capture only the number coordinates between the parentheses
   return match.group(1) if match else np.nan # Return only the coordinates; if the pattern is not found, return NaN
+
+def cost_prediction(route_df, year):
+  costs = route_df["nsmiles"] * 0.17507 * np.power(1.05, year-2019)
+  return costs
